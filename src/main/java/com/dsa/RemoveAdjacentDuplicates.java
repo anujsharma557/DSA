36610f439eb33e1f6a5086aae7d1ef33083dd044
Anuj <sharma.anuj557@gmail,.com>
2022-05-14 14:37:15 +0530

    removeAdjacentDuplicates

---
+++ src/main/java/com/dsa/RemoveAdjacentDuplicates.java
@@ -0,0 +1,31 @@
+package main.java.com.dsa;
+
+public class RemoveAdjacentDuplicates {
+
+    public static void main(String[] args){
+        int [] input={1,5,7,8,8,8,0,1,1,0,6,5};
+         int pointer=removeAdjacentDuplicates(input);
+         for(int i=0;i<=pointer;i++){
+             System.out.println(input[i]);
+         }
+    }
+
+    private static int removeAdjacentDuplicates(int[] input) {
+        int ptr=-1;
+        int i=0;
+        while(i<input.length ){
+            if(ptr==-1 || input[ptr]!=input[i]){
+                ptr++;
+                input[ptr]=input[i];
+                i++;
+            }else{
+                while(i<input.length && input[ptr]==input[i]){
+                    i++;
+                }
+               ptr--;
+            }
+        }
+        return ptr;
+
+    }
+}
