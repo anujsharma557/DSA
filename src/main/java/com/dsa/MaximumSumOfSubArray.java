5a5680f4e2e2bbc664039f6e061be7083f0872f9
Anuj <sharma.anuj557@gmail,.com>
2022-05-27 09:30:39 +0530

    maximumSumOfSubArray

---
+++ src/main/java/com/dsa/MaximumSumOfSubArray.java
@@ -0,0 +1,25 @@
+package main.java.com.dsa;
+
+/**
+ * @Author:sharma.anuj557@gmail.com
+ * @Description: maximum contiguous sum in an array
+ */
+public class MaximumSumOfSubArray{
+    public static void main(String[] args) {
+        int[] a={1,-3,4,-2,-1,6};
+        int sum=maximumSumOfSubArray(a);
+        System.out.println("maxSum:"+sum);
+    }
+
+    private static int maximumSumOfSubArray(int[] a) {
+        int len= a.length;
+        int sum=0;
+        int maxSum=a[0];
+        for(int i=0;i<len;i++){
+            sum= Math.max(sum+a[i],a[i]);
+            maxSum=Math.max(sum,maxSum);
+        }
+        return maxSum;
+
+    }
+}
