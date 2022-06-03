bf5c1b109f62920e21dd22803f3e7d9aa2a5695f
Anuj <sharma.anuj557@gmail,.com>
2022-06-03 17:24:07 +0530

    jumpGame

--- src/main/java/com/dsa/jumpGame.java
+++ src/main/java/com/dsa/jumpGame.java
@@ -3,7 +3,7 @@ package main.java.com.dsa;
 /**
  * @Author:sharma.anuj557@gmail.com
  * @Description: LeetCode #45 - Jump Game II
- * look for a better solution this can be done in O(N) also
+ * look for a better solution this can be done in O(N) also look at commented code below
  */
 public class jumpGame {
     public static void main(String[] args) {
@@ -50,4 +50,19 @@ class Solution {
         }
         return jumps;
     }
+
+    /*public int jump(int[] nums) {
+        int jumps = 0, currentJumpEnd = 0, farthest = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            // we continuously find the how far we can reach in the current jump
+            farthest = Math.max(farthest, i + nums[i]);
+            // if we have come to the end of the current jump,
+            // we need to make another jump
+            if (i == currentJumpEnd) {
+                jumps++;
+                currentJumpEnd = farthest;
+            }
+        }
+        return jumps;
+    }*/
 }
