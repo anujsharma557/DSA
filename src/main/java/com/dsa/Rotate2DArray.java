80be540be7350d855994804d9492f2fe046a2200
Anuj <sharma.anuj557@gmail,.com>
2022-06-04 13:17:00 +0530

    Rotate Image

--- src/main/java/com/dsa/Rotate2DArray.java
+++ src/main/java/com/dsa/Rotate2DArray.java
@@ -1,11 +1,40 @@
 package main.java.com.dsa;
 
+import java.util.Arrays;
+
+/**
+ * @Author:sharma.anuj557@gmail.com
+ * @Description: LeetCode #48 - Rotate Image
+ */
 public class Rotate2DArray {
     public static void main(String args[]) {
         int[][] arr = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-        printArr(arr);
-        System.out.println("");
-        rotateArray(arr);
+        rotate(arr);
+        Arrays.stream(arr).forEach(e->{
+            Arrays.stream(e).forEach(System.out::print);
+            System.out.println();
+        });
+    }
+
+    public static void rotate(int[][] matrix) {
+        int r=matrix.length;
+        int c= matrix[0].length;
+        int outerLoop=0;
+        if(r/2==0){
+            outerLoop=r/2;
+        }else{
+            outerLoop=r/2+1;
+        }
+
+        for(int i=0;i<outerLoop;i++){
+            for(int j=i;j<c-i-1;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[c-1-j][i] ;
+                matrix[c-1-j][i]=matrix[c-1-i][c-1-j];
+                matrix[c-1-i][c-1-j]= matrix[j][c-1-i];
+                matrix[j][c-1-i]=temp;
+            }
+        }
     }
 
     private static void rotateArray(int[][] arr) {
