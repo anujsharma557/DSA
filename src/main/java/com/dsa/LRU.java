e7d0ae053006c559596d5c02a90f4f900f96bdd5
Anuj <sharma.anuj557@gmail,.com>
2022-06-16 08:38:22 +0530

    LRU

---
+++ src/main/java/com/dsa/LRU.java
@@ -0,0 +1,150 @@
+package main.java.com.dsa;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Author:sharma.anuj557@gmail.com
+ * @Description: LeetCode #146 - LRU Cache
+ */
+/*
+public class LRU extends LinkedHashMap<Integer, Integer> {
+
+    private int capacity;
+
+    public LRU(int capacity) {
+        super(capacity, 0.75F, true);
+        this.capacity = capacity;
+    }
+
+    public int get(int key) {
+        return super.getOrDefault(key, -1);
+    }
+
+    public void put(int key, int value) {
+        super.put(key, value);
+    }
+
+    @Override
+    protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+        return size() > capacity;
+    }
+*/
+public class LRU{
+    public static void main(String[] args) {
+        LRU lru= new LRU(5);
+        lru.put(1,1);
+        lru.put(2,2);
+        lru.put(3,3);
+        lru.put(4,4);
+        lru.put(5,5);
+        lru.put(6,6);
+        lru.put(7,7);
+        lru.put(4,4);
+        lru.put(4,4);
+        lru.put(4,4);
+        lru.put(4,4);
+        lru.cache.values().stream().forEach(e->System.out.println(e.value));
+    }
+
+    class DLinkedNode {
+        int key;
+        int value;
+        DLinkedNode prev;
+        DLinkedNode next;
+    }
+
+    private void addNode(DLinkedNode node) {
+        /**
+         * Always add the new node right after head.
+         */
+        node.prev = head;
+        node.next = head.next;
+
+        head.next.prev = node;
+        head.next = node;
+    }
+
+    private void removeNode(DLinkedNode node){
+        /**
+         * Remove an existing node from the linked list.
+         */
+        DLinkedNode prev = node.prev;
+        DLinkedNode next = node.next;
+
+        prev.next = next;
+        next.prev = prev;
+    }
+
+    private void moveToHead(DLinkedNode node){
+        /**
+         * Move certain node in between to the head.
+         */
+        removeNode(node);
+        addNode(node);
+    }
+
+    private DLinkedNode popTail() {
+        /**
+         * Pop the current tail.
+         */
+        DLinkedNode res = tail.prev;
+        removeNode(res);
+        return res;
+    }
+
+    private Map<Integer, DLinkedNode> cache = new HashMap<>();
+    private int size;
+    private int capacity;
+    private DLinkedNode head, tail;
+
+    public LRU(int capacity) {
+        this.size = 0;
+        this.capacity = capacity;
+
+        head = new DLinkedNode();
+        // head.prev = null;
+
+        tail = new DLinkedNode();
+        // tail.next = null;
+
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    public int get(int key) {
+        DLinkedNode node = cache.get(key);
+        if (node == null) return -1;
+
+        // move the accessed node to the head;
+        moveToHead(node);
+
+        return node.value;
+    }
+
+    public void put(int key, int value) {
+        DLinkedNode node = cache.get(key);
+
+        if(node == null) {
+            DLinkedNode newNode = new DLinkedNode();
+            newNode.key = key;
+            newNode.value = value;
+
+            cache.put(key, newNode);
+            addNode(newNode);
+
+            ++size;
+
+            if(size > capacity) {
+                // pop the tail
+                DLinkedNode tail = popTail();
+                cache.remove(tail.key);
+                --size;
+            }
+        } else {
+            // update the value.
+            node.value = value;
+            moveToHead(node);
+        }
+    }
+}
