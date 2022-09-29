53ea7ab590afdcc84e7a23307b099118db1cb8e1
Anuj <sharma.anuj557@gmail,.com>
2022-09-29 11:20:33 +0530

    122. Best Time to Buy and Sell Stock II

--- src/main/java/com/dsa/BestTimetoBuyandSellStockII.java
+++ src/main/java/com/dsa/BestTimetoBuyandSellStockII.java
@@ -3,6 +3,11 @@ package main.java.com.dsa;
 /**
  * @Author:sharma.anuj557@gmail.com
  * @Description: LeetCode #122 - Best Time to Buy and Sell Stock II
+ * You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
+ *
+ * On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time. However, you can buy it then immediately sell it on the same day.
+ *
+ * Find and return the maximum profit you can achieve.
  */
 public class BestTimetoBuyandSellStockII {
     public static void main(String[] args) {
@@ -10,16 +15,12 @@ public class BestTimetoBuyandSellStockII {
         System.out.println(maxProfit(prices));
     }
     public static int maxProfit(int[] prices) {
-        int min=Integer.MAX_VALUE;
-        int maxDiff=0;
-
-        for(int i=0;i<prices.length;i++){
-            if(prices[i]<min){
-                min=prices[i];
-            }else{
-                maxDiff=Math.max(maxDiff,prices[i]-min);
+        int profit=0;
+        for(int i=1;i<prices.length;i++){
+            if(prices[i]-prices[i-1]>0){
+                profit+=  prices[i]-prices[i-1];
             }
         }
-        return maxDiff;
+        return profit;
     }
 }
